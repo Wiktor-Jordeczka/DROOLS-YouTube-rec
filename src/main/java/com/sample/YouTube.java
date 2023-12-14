2b9b47c65ff09ca740f89e51de2848916eb33346
Wiktor-Jordeczka <135605897+Wiktor-Jordeczka@users.noreply.github.com>
2023-12-14 15:48:58 +0100

    Code cleanup

--- src/main/java/com/sample/YouTube.java
+++ src/main/java/com/sample/YouTube.java
@@ -3,14 +3,9 @@ package com.sample;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.rule.QueryResults;
-import org.kie.api.runtime.rule.QueryResultsRow;
 import org.kie.api.logger.*;
 
-/**
- * This is a sample class to launch a rule.
- */
-public class DroolsTest {
+public class YouTube {
 
     public static final void main(String[] args) {
         try {
@@ -22,15 +17,6 @@ public class DroolsTest {
         	
             // go !
         	kSession.fireAllRules();
-        	
-        	/*// Nie wiem czy to ma sens skoro klasy są w .drl
-        	QueryResults results = kSession.getQueryResults( "Kanały do obejrzenia:" );
-        	for ( QueryResultsRow row : results ) {
-        	    Solution solution = ( Solution ) row.get( "solution" );
-        	    System.out.println( solution.getSolutionText() + "\n" );
-        	}
-        	*/
-        	
         	kLogger.close();
         } catch (Throwable t) {
             t.printStackTrace();
